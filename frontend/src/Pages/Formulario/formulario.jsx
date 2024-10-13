a955e19c02e6c7dc5051ef810ae48c60c9428f67
Ueslei Lima <uesleilima2003@gmail.com>
2024-10-13 16:48:23 -0300

    feat: Criando uma carrossel de imagens na primeira sessão da tela inicial.

--- frontend/src/Pages/Formulario/formulario.jsx
+++ frontend/src/Pages/Formulario/formulario.jsx
@@ -30,7 +30,6 @@ function Formulario() {
 
   return (
     <>
-    <Header />
     <body className='bodyForm'>
       <div className={`containerForm ${isSignUp ? 'active' : ''}`} id="container">
         {isSignUp ? (
