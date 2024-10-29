3b037832af80e3909282606991cc3d5ff5e260da
Maicon Cesar <133825394+MaiconCesar79@users.noreply.github.com>
2024-10-29 14:41:02 -0300

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,7 +35,7 @@ class UserController {
 
         const user = await usuarioModel.findOne({ where: { email: email } });
         if (!user) {
-            return res.status(400).json({ message: 'Usuário não encontrado!' });
+            return res.status(404).json({ message: 'Usuário não encontrado!' });
         }
  
         // Verifica se as senhas coincidem
@@ -66,7 +66,7 @@ class UserController {
             jwt.verify(token, secret);
             next();
         } catch (error) {
-            return res.status(404).json({ message: 'Token inválido!' });
+            return res.status(400).json({ message: 'Token inválido!' });
         }
     }
 }
