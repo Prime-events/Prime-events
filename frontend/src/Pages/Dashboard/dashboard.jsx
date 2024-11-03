cd7e9bc6c44cc0ec750d64cf89ae42a44921e213
Gabriel Lima <124406585+gbrljlima@users.noreply.github.com>
2024-11-02 23:54:25 -0300

    Feat - Criação SideBar

--- frontend/src/Pages/Dashboard/dashboard.jsx
+++ frontend/src/Pages/Dashboard/dashboard.jsx
@@ -1,9 +1,11 @@
 import SegundoHeader from "../../components/header/segundoHeader/segundoHeader";
+import SideBar from "../../components/sideBar/sideBar";
 
 function Dashboard() {
     return (
         <>
             <SegundoHeader titulo="Dashboard" />
+            <SideBar />
         </>
     );
 }
