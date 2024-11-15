010de1021ae8e659e06a282ea2682b88ae6d1b32
Gabriel Lima <124406585+gbrljlima@users.noreply.github.com>
2024-11-15 14:12:05 -0300

    Fix - Modelo Eventos + Post Criação eventos

---
+++ frontend/src/Pages/CriacaoEvento/api.js
@@ -0,0 +1,14 @@
+const API_URL = 'http://localhost:3001/eventos';
+
+
+export const createEvento = async (evento) => {
+    await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(evento),
+      
+    });
+    console.log("Enviado");
+  };
