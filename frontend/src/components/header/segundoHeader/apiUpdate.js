be8b7d40a542db628d7c2d13bb3e4bb62c6c29f3
Ueslei Lima <uesleilima2003@gmail.com>
2024-11-16 19:26:20 -0300

    feat: Adicionando funcionalidade de alteração do email e senha.

--- frontend/src/components/header/segundoHeader/apiUpdate.js
+++ frontend/src/components/header/segundoHeader/apiUpdate.js
@@ -1,4 +1,6 @@
 const API_URL = 'http://localhost:3001/api/updateUser';
+const updateEmail_URL = 'http://localhost:3001/api/updateEmail';
+const updateSenha_URL = 'http://localhost:3001/api/updateSenha';
 
 export const updateUser = async (email, updates) => {
     try {
@@ -20,3 +22,52 @@ export const updateUser = async (email, updates) => {
         throw error;
     }
 };
+export const updateUserEmail = async (email, atualizacoes) => {
+    try {
+        const response = await fetch(`${updateEmail_URL}/${encodeURIComponent(email)}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(atualizacoes), // Enviando os dados a serem atualizados
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            const error = new Error(errorData.message || 'Erro ao atualizar email');
+            error.response = response;  // Anexa a resposta ao erro para acessarmos depois
+            throw error;
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error("Erro na API:", error);
+        throw error;
+    }
+};
+
+export const updateUserSenha = async (email, atualizacoes) => {
+    try {
+        const response = await fetch(`${updateSenha_URL}/${encodeURIComponent(email)}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(atualizacoes), // Enviando os dados a serem atualizados
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            const error = new Error(errorData.message || 'Erro ao atualizar email');
+            error.response = response;  // Anexa a resposta ao erro para acessarmos depois
+            throw error;
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error("Erro na API:", error);
+        throw error;
+    }
+};
+
+
