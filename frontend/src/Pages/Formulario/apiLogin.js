583168e345a219bdb69eeae9d046b270b079682a
Ueslei Lima <145942952+UesleiLM@users.noreply.github.com>
2024-11-18 16:27:45 -0300

    feat: Add toastfy no formulário de cadastro e login. (#40)

--- frontend/src/Pages/Formulario/apiLogin.js
+++ frontend/src/Pages/Formulario/apiLogin.js
@@ -1,13 +1,25 @@
 const API_URL_LOGIN = 'http://localhost:3001/api/Login';
 
 export const loginUser = async (user) => {
-    const response = await fetch(API_URL_LOGIN, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-    });
-    return response;
+    try {
+        const response = await fetch(API_URL_LOGIN, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        });
+        if (!response.ok) {
+            const errorData = await response.json();
+            const error = new Error(errorData.message || 'Erro ao fazer login');
+            error.response = response;  // Anexa a resposta ao erro para acessarmos depois
+            throw error;
+        }
+        return response;
+    } catch (error) {
+        console.error("Erro na API:", error);
+        throw error;
+    }
+
 };
 
