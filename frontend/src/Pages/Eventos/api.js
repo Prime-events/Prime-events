1ee77998a364b0ea75e4b0a5407b08982608088d
Ueslei Lima <uesleilima2003@gmail.com>
2024-12-08 18:59:37 -0300

    feat: Adicionando filtros em eventos.

--- frontend/src/Pages/Eventos/api.js
+++ frontend/src/Pages/Eventos/api.js
@@ -1,4 +1,6 @@
+import axios from 'axios';
 const API_URL_EVENTOS = 'http://localhost:3001/eventos';
+const API_URL_EVENTOS_NOME = 'http://localhost:3001/eventos';
 
 export const listarEventosUsuario = async (idUsuario) => {
   try {
@@ -31,6 +33,41 @@ export const listarEvento = async (idEvento) => {
     return await response.json();
   } catch (error) {
     console.error("Erro na API:", error);
+    throw error;
+  }
+};
+
+
+export const listarEventoPorNome = async (nomeEvento) => {
+  try {
+    const response = await fetch(`${API_URL_EVENTOS_NOME}/nome/${encodeURIComponent(nomeEvento)}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Erro ao buscar Evento");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Erro na API:", error);
+    throw error;
+  }
+};
+
+
+export const buscarEventosPorPeriodo = async (dataInicial, dataFinal) => {
+  try {
+    const response = await axios.get('/api/eventos/filtro', {
+      params: {
+        dataInicial,
+        dataFinal
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Erro na busca de eventos:', error);
     throw error;
   }
 };
